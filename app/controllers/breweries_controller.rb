fd2e6551763b4ca087a1f95acde5f2e985e6bf05
outisa <savolainen.outi@gmail.com>
2022-12-29 14:49:39 +0200

    Fix lint

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -51,11 +51,11 @@ class BreweriesController < ApplicationController
 
   def toggle_activity
     brewery = Brewery.find(params[:id])
-    brewery.update_attribute :active, (not brewery.active)
-  
+    brewery.update_attribute :active, !brewery.active
+
     new_status = brewery.active? ? "active" : "retired"
-  
-    redirect_to brewery, notice:"brewery activity status changed to #{new_status}"
+
+    redirect_to brewery, notice: "brewery activity status changed to #{new_status}"
   end
 
   # DELETE /breweries/1 or /breweries/1.json
